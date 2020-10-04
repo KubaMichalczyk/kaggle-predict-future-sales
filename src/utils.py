eab835b8deb7b72aa4e2987eee76b8e565e2e434
Kuba <kuba.michalczyk@gmail.com>
2020-10-04 07:32:33 +0200

    Added docstrings to GroupTimeSeriesSplit()

--- src/utils.py
+++ src/utils.py
@@ -4,15 +4,46 @@ from sklearn.utils.validation import _num_samples
 from sklearn.model_selection._split import _BaseKFold
 
 class GroupTimeSeriesSplit(_BaseKFold):
+    """
+    Time Series cross-validator for a variable number of observations within the time unit.
+    In the kth split, it returns first k folds as train set and the (k+1)th fold as test set.
+    Indices can be grouped so that they enter the CV fold together.
+
+    Parameters
+    ----------
+    n_splits : int, default=5
+        Number of splits. Must be at least 2.
+    max_train_size : int, default=None
+        Maximum size for a single training set.
+    """
     def __init__(self, n_splits=5, *, max_train_size=None):
         super().__init__(n_splits, shuffle=False, random_state=None)
         self.max_train_size = max_train_size
 
     def split(self, X, y=None, groups=None):
+        """
+        Generate indices to split data into training and test set.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data, where n_samples is the number of samples and n_features is the number of features.
+        y : array-like of shape (n_samples,)
+            Always ignored, exists for compatibility.
+        groups : array-like of shape (n_samples,)
+            Group labels for the samples used while splitting the dataset into train/test set.
+            Most often just a time feature.
+
+        Yields
+        -------
+        train : ndarray
+            The training set indices for that split.
+        test : ndarray
+            The testing set indices for that split.
+        """
         n_splits = self.n_splits
         X, y, groups = indexable(X, y, groups)
         n_samples = _num_samples(X)
-        n_splits = self.n_splits
         n_folds = n_splits + 1
         indices = np.arange(n_samples)
         group_counts = np.unique(groups, return_counts=True)[1]
